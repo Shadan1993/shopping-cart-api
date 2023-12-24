ae12298256a4cadc154c23ba90cdf443e180029a
Naser Azadbakht <naserazt27@gmail.com>
2023-12-24 17:53:06 +0330

    prs shoppping cart api server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const orderRoutes = require("./Routes/orderRoutes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", orderRoutes);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/orders")
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
